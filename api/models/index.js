19b64356c3efa7f5667f2a399107fbccbe3969fc
carollindaa <caroline593142@gmail.com>
2021-12-06 10:22:48 -0300

    u

--- api/models/index.js
+++ api/models/index.js
@@ -11,13 +11,20 @@ app.get('/bitchs', async(req, resp) => {
 
 app.post('/bitchs', async(req, resp) => {
     try{
-        let {id_nome} = req.body; 
+        let {nome, chamada, curso, turma} = req.body; 
 
-        let r = await db.tb_lista_negra.create({
-            id_nome 
+        let r = await db.tb_matricula.create({
+            nm_aluno : nome, 
+            nr_chamada : chamada, 
+            nm_curso : curso,
+            nm_turma : turma 
         })
 
         resp.send(r); 
 
     }catch (e) {
         resp.send({ erro: e.toString()})
+
+    }
+
+})
